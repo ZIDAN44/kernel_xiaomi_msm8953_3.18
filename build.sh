1331c46c2af73219d11e68c33c1192f25dd5f20f
RevanthTolety <crevanthtolety@gmail.com>
2019-07-27 03:23:42 +0000

    kernel: Import a Build Script

---
+++ build.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/ChalapathiRevanth/Toolchain ~/Toolchain
+export ARCH=arm64
+export CROSS_COMPILE=~/Toolchain/bin/aarch64-linux-android-
+export KBUILD_BUILD_USER=RevanthTolety
+export KBUILD_BUILD_HOST=CR-buildserver
+mkdir output
+make -C $(pwd) O=output sakura_defconfig
+make -j32 -C $(pwd) O=output
